61676d0a3f3e09345428ba5545aa6091b2dce354
Martin Tellez Falcón <martintellezfalcon@gmail.com>
2026-03-08 13:39:23 -0600

    Correxion de errores en obtener_usuario.php

--- obtener_usuarios.php
+++ obtener_usuarios.php
@@ -1,11 +1,11 @@
 <?php
-// obtener_usuarios.php
-include 'db.php'; // Asegúrate de tener tu conexión a BD aquí
 header('Content-Type: application/json');
+
+include 'db.php';
 
 try {
     $stmt = $pdo->query("SELECT id_usuario, nombre, correo FROM usuarios");
-    $usuarios = $stmt->fetchAll(PDO::ErrorMode => PDO::FETCH_ASSOC);
+    $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($usuarios);
 } catch (Exception $e) {
     echo json_encode(['error' => $e->getMessage()]);
